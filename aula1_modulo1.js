9aa95dc3c290d8fea3dafce9acb0e102a810ad78
igorSguissardi <159180268+igorSguissardi@users.noreply.github.com>
2024-02-07 16:22:37 -0300

    ponderada1CristianoSemana1-IgorSguissardi

---
+++ aula1_modulo1.js
@@ -0,0 +1,10 @@
+// Controle de Notas de Alunos
+var nota1 = 9
+var nota2 = 7
+var soma = (nota1 + nota2)
+var media = soma/2
+
+if (media >= 7) {
+    console.log("Você passou no test :-)")}
+    else {
+    console.log("você reprovou no test :-(")}
